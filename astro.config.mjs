387b8775fdaadbaec629e5a6992751c209c4abe9
marks <mark@botable.io>
2025-02-01 05:01:13 -0700

    Fixed sitemap config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
@@ -11,5 +12,6 @@ export default defineConfig({
 		tailwind({
 			configFile: './tailwind.config.cjs',
 		}),
+		sitemap(),
 	],
 });
